1c38a2d2cd371cee8f29c7801dbc39f86c43131e
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-20 23:30:29 -0700

    add moment js on due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +15,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -151,9 +155,7 @@ $(".list-group").on("click", "span", function() {
 // value of due date was changed
 $(".list-group").on("change", "input[type='text']", function () {
   // get current text
-  var date = $(this)
-  .val()
-  .trim();
+  var date = $(this).val();
 
   // get the parent ul's id attribute
   var status = $(this)
@@ -175,8 +177,8 @@ $(".list-group").on("change", "input[type='text']", function () {
   .addClass("badge badge-primary badge-pill")
   .text(date);
 
-  // replace input with span element
-  $(this).replaceWith(taslSpan);
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -251,6 +253,27 @@ $("#trash").droppable({
   }
 
 });
+
+var auditTask = function(taskEl) {
+  // get date from task e lement
+  var date = $(taskEl).find("span").text().trim();
+  // ensure it worked
+  console.log(date);
+
+  // convert to moment object at 5:00 pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 $("#modalDueDate").datepicker({
   minDate: 1
